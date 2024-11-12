ccece3db99b80cd732364d52aacf26d2be4c6189
Chamber6821 <vania.osinin@yandex.ru>
2024-11-12 04:36:28 +0300

    Add FakeUsers

--- src/users/Users.ts
+++ src/users/Users.ts
@@ -4,3 +4,13 @@ import User from "../user/User";
 export default interface Users {
   withId(id: UserId): Promise<User>;
 }
+
+export class FakeUsers implements Users {
+  async withId(id: UserId): Promise<User> {
+    return {
+      id: async () => id,
+      name: async () => `User ${id}`,
+      subscrption: async () => undefined
+    }
+  }
+}
