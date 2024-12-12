e2585cf1adbaa1ebb7dfc2c78862ad433f6a3dc4
Konstantin Baranchuk <kos5anchik@gmail.com>
2024-12-12 20:54:33 +0300

    feat: settingService

---
+++ src/setting/setting.ts
@@ -0,0 +1,38 @@
+import {PrismaClient} from "@prisma/client";
+
+export default class SettingService {
+  constructor(
+    private readonly prisma: PrismaClient
+  ) {}
+
+  async getSetting(key: string): Promise<boolean> {
+    const text = await this.prisma.setting.findUnique({ where: { key: key } });
+    if (!text) return false;
+    console.log(typeof text.value);
+    if (typeof text.value === "boolean") {
+      console.log(text.value);
+      return text.value;
+    }
+    return false;
+  }
+
+  async setSetting(key: string, value: boolean) {
+    await this.prisma.setting.upsert({
+      where: { key: key },
+      create: { key: key, value: value },
+      update: { value: value }
+    })
+  }
+
+  async getReferrals() {
+    return await this.getSetting('referrals');
+  }
+
+  async getTypes() {
+    return await this.getSetting('types');
+  }
+
+  async getAskFrom() {
+    return await this.getSetting('ask-from');
+  }
+}
