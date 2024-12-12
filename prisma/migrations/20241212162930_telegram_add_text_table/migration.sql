e2ad12159c0a06818185fad8a75d26549d735a90
Konstantin Baranchuk <kos5anchik@gmail.com>
2024-12-12 19:51:41 +0300

    feat: textService

---
+++ prisma/migrations/20241212162930_telegram_add_text_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Text" (
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "Text_pkey" PRIMARY KEY ("key")
+);
---
+++ src/text/text.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+
+export default class TextService {
+  constructor(
+    private readonly prisma: PrismaClient,
+  ) {}
+  async getField(key: string): Promise<string> {
+    const text = await this.prisma.text.findUnique({ where: { key: key } });
+    if (!text) return ' ';
+    return text.value;
+  }
+
+  async setField(key: string, value: string) {
+    await this.prisma.text.upsert({
+      where: { key: key },
+      create: { key: key, value: value },
+      update: { value: value }
+    })
+  }
+
+  async getSupport() {
+    return await this.getField('support');
+  }
+
+  async getDropShipping() {
+    return await this.getField('dropShipping');
+  }
+
+  async getInstruction() {
+    return await this.getField('instruction');
+  }
+}
